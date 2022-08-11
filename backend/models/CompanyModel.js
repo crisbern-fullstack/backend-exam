3aadef57c4aa05c0361f0151250d5505ac2d63e7
Crisbern Gozun <crisberngozun022@gmail.com>
2022-08-11 12:26:32 +0800

    code formatting

--- backend/models/CompanyModel.js
+++ backend/models/CompanyModel.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose')
-const {
-    customEmailValidator,
-    urlValidator
-} = require('./custom-validators')
-const validator = require('validator')
+const mongoose = require("mongoose");
+const { customEmailValidator, urlValidator } = require("./custom-validators");
+const validator = require("validator");
 
-const Schema = mongoose.Schema
+const Schema = mongoose.Schema;
 
 /* Company Schema fields are as follows
 1. Name (required)
@@ -13,20 +10,23 @@ const Schema = mongoose.Schema
 3. logo (min. 100x100 in size and maximun suze of 2mb) (this will be a path instead of the image itself)
 4. website (add url validators)
 */
-const CompanySchema = new Schema({
-    name : {
-        type : String,
-        required : [true, "Company name is required."]
+const CompanySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Company name is required."],
     },
-    email : {
-        type : String,
-        validate: [customEmailValidator, "Invalid email."]
+    email: {
+      type: String,
+      validate: [customEmailValidator, "Invalid email."],
     },
-    logo : String,
-    website : {
-        type : String,
-        validate : [urlValidator, "Invalid Company URL"]
-    }
-}, {timestamps:true})
+    logo: String,
+    website: {
+      type: String,
+      validate: [urlValidator, "Invalid Company URL"],
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("CompanySchema", CompanySchema)
+module.exports = mongoose.model("CompanySchema", CompanySchema);
