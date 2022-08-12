941ba78619447fe23492fc5376429f953ecd8106
Crisbern T. Gozun <crisbern@fullstack.ph>
2022-08-12 11:27:34 +0800

    employee edit fixed

--- backend/controllers/DataQueryController.js
+++ backend/controllers/DataQueryController.js
@@ -232,7 +232,8 @@ const UpdateEmployee = async (req, res) => {
   try {
     const employee = await EmployeeModel.findOneAndUpdate(
       { _id: id },
-      { ...req.body }
+      { ...req.body },
+      { new: true }
     );
     if (!employee) {
       return res.status(404).json({ error: "Employee not found" });
