6203afa3e618a4281517a8bca03c190fcda21cc3
Crisbern Gozun <crisberngozun022@gmail.com>
2022-08-13 22:33:37 +0800

    some form validations

--- backend/controllers/DataQueryController.js
+++ backend/controllers/DataQueryController.js
@@ -285,11 +285,13 @@ const UpdateEmployee = async (req, res) => {
     const employee = await EmployeeModel.findOneAndUpdate(
       { _id: id },
       { ...req.body },
-      { new: true }
+      { new: true, runValidators: true }
     );
+
     if (!employee) {
       return res.status(404).json({ error: "Employee not found" });
     }
+
     return res.status(200).json(employee);
   } catch (error) {
     return res.status(400).json({ error: error.message });
